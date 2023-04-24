2ba82e13ee74ec50df31a272ae436bc71f6ea71c
davidenkoiuliia <115422266+davidenkoiuliia@users.noreply.github.com>
2023-04-25 00:18:12 +0300

    Add files via upload

---
+++ Exceptions/IO/InvalidFileDataException.java
@@ -0,0 +1,6 @@
+package Exceptions.IO;
+
+public class InvalidFileDataException extends Exception {
+    public InvalidFileDataException(String path, String message) {
+        super("! invalid file data in " + path + ": " + message + " !");
+    }

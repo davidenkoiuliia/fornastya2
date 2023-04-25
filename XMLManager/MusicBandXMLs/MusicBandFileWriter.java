e1822535da709f340a475e318cdc5d79ec83ba6f
Nastya Semenova8888888 <115896272+nastysem@users.noreply.github.com>
2023-04-25 14:16:24 +0300

    Delete MusicBandFileWriter.java

--- XMLManager/MusicBandXMLs/MusicBandFileWriter.java
+++
@@ -1,9 +0,0 @@
-package XMLManager.MusicBandXMLs;
-
-import Exceptions.IO.FilePermissionException;
-import Exceptions.IO.CustomIOException;
-import java.io.FileNotFoundException;
-
-public interface MusicBandFileWriter {
-    void write(MusicBandFileWriter movieCollection) throws FileNotFoundException, FilePermissionException, CustomIOException;
-}

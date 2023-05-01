edac9804f17e14bae3c63cc6525957e865bfece7
davidenkoiuliia <115422266+davidenkoiuliia@users.noreply.github.com>
2023-05-01 16:35:47 +0300

    Add files via upload

---
+++ Client/PrettyPrinter.java
@@ -0,0 +1,83 @@
+package Client;
+
+import Models.Band;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.List;
+
+
+public class PrettyPrinter {
+    static final String keyFormat = "%5s |";
+    static final String bandIDFormat = "%5s |";
+    static final String bandNameFormat = "%15s |";
+    static final String XFormat = "%7s |";
+    static final String YFormat = "%7s |";
+    static final String dateFormat = "%15s |";
+    static final String albumsCountFormat = "%7s |";
+    static final String genreFormat = "%9s |";
+    static final String numberOfParticipantsFormat = "%7s |";
+    static final String studioName = "%12s |";
+    static final String studioAddress = "%20s |";
+    static void printBandHashMap(HashMap<Integer, Band> hashMap) {
+        System.out.printf(keyFormat, "Key");
+        printBandParamNames();
+        System.out.println("-".repeat(165));
+
+        for (Integer key : hashMap.keySet()) {
+            Band band = hashMap.get(key);
+            System.out.printf(keyFormat, key);
+            printBandParams(band);
+        }
+    }
+    public static void printBandList(List<Band> bandList) {
+        printBandParamNames();
+        System.out.println("-".repeat(158));
+
+        for (Band band : bandList) {
+            printBandParams(band);
+        }
+    }
+    public static void printBandListAlbumsCount(List<Band> bandList) {
+        System.out.printf(bandIDFormat, "ID");
+        System.out.printf(bandNameFormat, "movieName");
+        System.out.printf(albumsCountFormat, "albums count");
+        System.out.println();
+        System.out.println("-".repeat(33));
+
+        for (Band band : bandList) {
+            System.out.printf(bandIDFormat, band.getId());
+            System.out.printf(bandNameFormat, band.getName());
+            System.out.printf(albumsCountFormat, band.getAlbumsCount());
+            System.out.println();
+        }
+    }
+
+
+    private static void printBandParamNames() {
+        System.out.printf(bandIDFormat, "ID");
+        System.out.printf(bandNameFormat, "bandName");
+        System.out.printf(XFormat, "coordX");
+        System.out.printf(YFormat, "coordY");
+        System.out.printf(dateFormat, "creationDate");
+        System.out.printf(numberOfParticipantsFormat, "number of participants");
+        System.out.printf(genreFormat, "genre");
+        System.out.printf(albumsCountFormat, "albums count");
+        System.out.printf(studioName, "stdName");
+        System.out.printf(studioAddress, "stdAddress");
+        System.out.println();
+    }
+
+    private static void printBandParams(Band band) {
+        System.out.printf(bandIDFormat, band.getId());
+        System.out.printf(bandNameFormat, band.getName());
+        System.out.printf(XFormat, band.getCoordinates().getX());
+        System.out.printf(YFormat, band.getCoordinates().getY());
+        System.out.printf(dateFormat, band.getCreationDate().format(DateTimeFormatter.ofPattern(dateFormat)));
+        System.out.printf(albumsCountFormat, band.getAlbumsCount());
+        System.out.printf(genreFormat, band.getGenre());
+        System.out.printf(numberOfParticipantsFormat, band.getNumberOfParticipants());
+        System.out.printf(studioName, band.getStudio().getName());
+        System.out.printf(studioAddress, band.getStudio().getAddress());
+        System.out.println();
+    }
+}

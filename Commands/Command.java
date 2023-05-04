76308049144354a24e0e00edb886ef5eb5c9af35
Nastya Semenova8888888 <115896272+nastysem@users.noreply.github.com>
2023-05-04 15:43:58 +0300

    Delete Commands directory

--- Commands/Command.java
+++
@@ -1,9 +0,0 @@
-package Commands;
-
-import Exceptions.IO.CustomIOException;
-import Exceptions.IO.WrongArgumentException;
-import Exceptions.Receiver.CollectionKeyException;
-
-public interface Command {
-    void execute() throws CollectionKeyException, WrongArgumentException, CustomIOException;
-}
--- Commands/CommandWithResult.java
+++
@@ -1,5 +0,0 @@
-package Commands;
-
-public interface CommandWithResult<T> extends Command {
-    T getResult();
-}

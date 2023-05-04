706dc5b4605f4727f74d1c4f934a473b835bb8ec
Nastya Semenova8888888 <115896272+nastysem@users.noreply.github.com>
2023-05-04 17:08:30 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+import Client.ConsoleClient;
+import Reciever.MusicBandCollection;
+
+public class Main {
+    public static void main(String[] args) {
+        ConsoleClient consoleClient = new ConsoleClient();
+        consoleClient.main();
+    }
+}
